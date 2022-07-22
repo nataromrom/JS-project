4fe0cb5e9a69a2965c1b710c1843a7ab302c056e
alinelina <alinelina2013@gmail.com>
2022-07-22 23:46:41 +0300

    added start page

--- src/app.js
+++ src/app.js
@@ -2,4 +2,5 @@ import './js/main';
 import './js/start';
 import './js/table';
 import './scss/base.scss';
-import './scss/normalize.scss'
+import './scss/normalize.scss';
+import './scss/start.scss';
